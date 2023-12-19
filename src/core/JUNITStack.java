af46508ef7a24ad7bebb937a0dd8c36d4fcb7abc
pakin6509681182 <pakin.pra@dome.tu.ac.th>
2023-12-19 15:47:26 +0700

    Remove commented out testPushDifferentElmTypeToStack method

--- src/core/JUNITStack.java
+++ src/core/JUNITStack.java
@@ -24,21 +24,6 @@ public class JUNITStack extends TestCase {
         Integer topElement = s1.top();
         assertEquals(Integer.valueOf(1), topElement);
     }
-    /*public void testPushDifferentElmTypeToStack() {
-        Stack s1 = new Stack();
-        s1.push(1);
-        try {
-            s1.push(null);
-            //fail("Expected an IllegalArgumentException to be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("All elements in the stack must be of the same type.", e.getMessage());
-        }
-
-        int size = s1.getSize();
-        assertEquals(1, size);
-        Integer topElement = s1.top();
-        assertEquals(Integer.valueOf(1), topElement);
-    }*/
     public void testLastInFirstOut() {
         Stack s1 = new Stack();
         s1.push(1);
